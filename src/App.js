b8a9f46312d887d9c21b68cebe6abb179a2c6226
ispoljari <ivanspoljaric22@gmail.com>
2019-05-22 18:19:10 +0200

    Init app with starter files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+// test code
+
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    response: '',
+    error: ''
+  };
+
+  componentDidMount() {
+    this.fetchDataFromAPI();
+  }
+
+  fetchDataFromAPI = async () => {
+    try {
+      const apiRawData = await fetch('/api/users');
+      if (!apiRawData.ok) {
+        throw new Error('An error has occured!');
+      }
+      const apiJSONData = await apiRawData.text();
+      this.processResponse(apiJSONData);
+    } catch (error) {
+      this.processError(error.message);
+    }
+  };
+
+  processResponse = response => {
+    this.setState({
+      response,
+      error: ''
+    });
+  }
+
+  processError = error => {
+    this.setState({
+      response: '',
+      error
+    });
+  }
+
+  render() {
+    const { response, error } = this.state;
+
+    return (
+      <div className="app-container">
+        <header role="banner" className="app-container_header">
+          <h1>
+            Rezervacija Dvorana u Križevcima
+          </h1>
+        </header>
+        <main role="main" className="app-container_main">
+          <p>{response ? response : error}</p>
+        </main>
+        <footer role="contentinfo" className="app-container_footer">
+          <p>
+            Dev. by Speck Academy 2019.
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
