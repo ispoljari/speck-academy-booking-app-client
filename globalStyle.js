770977b03114f226b0be4e6f7013ddbdbbd2cb1d
ispoljari <ivanspoljaric22@gmail.com>
2019-05-27 14:25:46 +0200

    Install styled-components and create a globaL style

---
+++ globalStyle.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+  box-sizing: border-box;
+  }
+
+  * {
+    margin: 0;
+  }
+
+  *, *:after, *:before {
+    box-sizing: inherit;
+  }
+`;
+
+
+export default GlobalStyle;
