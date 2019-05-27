56c05fb7a61a006f64c1267cc1e04c6ffac9c292
ispoljari <ivanspoljaric22@gmail.com>
2019-05-27 14:15:08 +0200

    Remove boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,66 +1,9 @@
-// test code
+import React from 'react';
 
-import React, { Component } from 'react';
-import './App.css';
-
-class App extends Component {
-  state = {
-    response: '',
-    error: ''
-  };
-
-  componentDidMount() {
-    this.fetchDataFromAPI();
-  }
-
-  fetchDataFromAPI = async () => {
-    try {
-      const apiRawData = await fetch('/api/users');
-      if (!apiRawData.ok) {
-        throw new Error('An error has occured!');
-      }
-      const apiJSONData = await apiRawData.text();
-      this.processResponse(apiJSONData);
-    } catch (error) {
-      this.processError(error.message);
-    }
-  };
-
-  processResponse = response => {
-    this.setState({
-      response,
-      error: ''
-    });
-  }
-
-  processError = error => {
-    this.setState({
-      response: '',
-      error
-    });
-  }
-
-  render() {
-    const { response, error } = this.state;
-
-    return (
-      <div className="app-container">
-        <header role="banner" className="app-container_header">
-          <h1>
-            Rezervacija Dvorana u Križevcima
-          </h1>
-        </header>
-        <main role="main" className="app-container_main">
-          <p>{response ? response : error}</p>
-        </main>
-        <footer role="contentinfo" className="app-container_footer">
-          <p>
-            Dev. by Speck Academy 2019.
-          </p>
-        </footer>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    Hello World!
+  </div>
+);
 
 export default App;
