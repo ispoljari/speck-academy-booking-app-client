51f32c20e2e86b0391323466540e4a0bddd4f4a6
ispoljari <ivanspoljaric22@gmail.com>
2019-05-27 14:32:34 +0200

    Import Normalize and add to App comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Normalize } from 'styled-normalize';
 
 import GlobalStyle from './globalStyle';
 
 const App = () => (
   <React.Fragment>
+    <Normalize />
     <GlobalStyle />
     <h1>Hello World!</h1>
   </React.Fragment>
