c37a1ce6d113033f1fe17cb96aa9c3141c163c78
dodge1912 <ivang1912@gmail.com>
2019-05-30 18:36:59 +0200

    export CitizensAdminLogin function

--- src/UI/citizens/index.js
+++ src/UI/citizens/index.js
@@ -1,8 +1,20 @@
 export { default as CitizensPage } from './pages/CitizensPage';
 export { default as CitizensHeader } from './components/header/CitizensHeader';
-export { default as CitizensSelectHall } from './components/select-hall/CitizensSelectHall';
-export { default as CitizensHallInfo } from './components/hall-info/CitizensHallInfo';
-export { default as CitizensSelectDateTime } from './components/select-date-time/CitizensSelectDateTime';
-export { default as CitizensEditEventInfo } from './components/edit-event-info/CitizensEditEventInfo';
-export { default as CitizensSubmitRequest } from './components/submit-request/CitizensSubmitRequest';
-
+export {
+  default as CitizensSelectHall
+} from './components/select-hall/CitizensSelectHall';
+export {
+  default as CitizensHallInfo
+} from './components/hall-info/CitizensHallInfo';
+export {
+  default as CitizensSelectDateTime
+} from './components/select-date-time/CitizensSelectDateTime';
+export {
+  default as CitizensEditEventInfo
+} from './components/edit-event-info/CitizensEditEventInfo';
+export {
+  default as CitizensSubmitRequest
+} from './components/submit-request/CitizensSubmitRequest';
+export {
+  default as CitizensAdminLogin
+} from './components/login/CitizensAdminLogin';
