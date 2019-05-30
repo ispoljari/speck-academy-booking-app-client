b3957d73ec39f26f9b99020377235f6240f37d57
Goran Ljaljić <gljaljic@gmail.com>
2019-05-30 17:47:57 +0200

    Coppied code from develop-branch.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from "react";
+/* import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import EditHallComponent from './UI/admin/components/hall-reservation/AdminHallReservation.js';
+
+ReactDOM.render(<EditHallComponent />, document.getElementById('root'));
