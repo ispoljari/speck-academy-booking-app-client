91d5ea7eff082129e025b41205640b5e9b68de6c
dodge1912 <ivang1912@gmail.com>
2019-05-30 18:47:06 +0200

    add style for components

---
+++ src/UI/citizens/components/login/CitizensAdminLoginStyle.js
@@ -0,0 +1,121 @@
+import styled from 'styled-components';
+
+const LoginContainer = styled.div`
+  margin: auto;
+  width: 632px;
+  height: 264px;
+  border-radius: 4px;
+  border: solid 1px #d9d9d9;
+  background-color: #ffffff;
+`;
+
+const DataContainer = styled.div`
+  background-colour: yellow;
+  margin: 32px;
+`;
+
+const TitleAdminLogin = styled.div`
+  width: 568px;
+  height: 40px;
+  font-family: Lora;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+`;
+
+const FormComponent = styled.div`
+  display: inline-block;
+  width: 282px;
+  heihgt: 64px;
+  margin-top: 24px;
+`;
+
+const FormComponent2 = styled.div`
+  display: inline-block;
+  width: 282px;
+  heihgt: 64px;
+  margin-top: 24px;
+`;
+
+const FormComponentName = styled.div`
+  width: 185px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+`;
+
+const FormComponentInput = styled.input`
+  margin-top: 8px;
+  width: 272px;
+  height: 32px;
+  border-radius: 2px;
+  border: solid 1px rgba(0, 0, 0, 0.15);
+`;
+
+const LoginSubmitButton = styled.button`
+  margin-top: 25px;
+  margin-left: 106px;
+  width: 356px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: #0f4951;
+`;
+
+const LoginSubmitButtonText = styled.p`
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ffffff;
+  margin: auto;
+`;
+
+const FormComponentName2 = styled.div`
+  width: 185px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 16px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+  margin-left: 8px;
+`;
+
+const FormComponentInput2 = styled.input`
+  margin-top: 8px;
+  width: 272px;
+  height: 32px;
+  border-radius: 2px;
+  border: solid 1px rgba(0, 0, 0, 0.15);
+  margin-left: 8px;
+`;
+
+export {
+  LoginContainer,
+  DataContainer,
+  TitleAdminLogin,
+  FormComponent,
+  FormComponentName,
+  FormComponentInput,
+  LoginSubmitButton,
+  LoginSubmitButtonText,
+  FormComponent2,
+  FormComponentName2,
+  FormComponentInput2
+};
