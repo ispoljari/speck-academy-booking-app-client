fcbe0d95bb4c820f7f3bcc66cf6794ad6d68dd39
Ivica Sirjan <isirjan@gmail.com>
2019-05-30 18:34:47 +0200

    import React from 'react';
    import {
        CitizensEditEventInfoContainer,
        CitizensEditEventInfoHeader,
        CitizensEditEventInfoDescription,
     } from './CitizensEditEventInfoStyle';
    
     class CitizensEditEventInfo extends React.Component {
        render() {
          return (
            <CitizensEditEventInfoContainer>
                <CitizensEditEventInfoHeader>
                    Podaci o događaju i Vama
                </CitizensEditEventInfoHeader>
                <CitizensEditEventInfoDescription>
                    Recite nam ime svog događaja i pobliže ga objasnite. Nemojte zaboraviti ostaviti svoje kontakte podatke kako bismo Vas mogli kontaktirati u vezi dodatnih detalja i potvrđivanja rezervacije.
                </CitizensEditEventInfoDescription>
                <form>
                    <label>
                        Naziv događaja:
                    </label>
                    <br />
                    <input type="text" />
                </form>
                <form>
                    <label>
                        Opis događaja:
                    </label>
                    <br />
                    <input type="text" value="Opišite svoj događaj..." />
                </form>
            </CitizensEditEventInfoContainer>
          );
        }
      }
    
    export default CitizensEditEventInfo;

---
+++ src/UI/citizens/components/edit-event-info/CitizensEditEventInfoStyle.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+const CitizensEditEventInfoContainer = styled.div`
+  width: 1024px;
+  height: auto;
+  border-radius: 4px;
+  border: solid 1px #d9d9d9;
+  background-color: #ffffff;
+  padding-left: 130px;
+  padding-right: 130px;
+`;
+
+const CitizensEditEventInfoHeader = styled.h1`
+  font-family: Lora;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+`;
+
+const CitizensEditEventInfoDescription = styled.p`
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+`;
+
+export {
+  CitizensEditEventInfoContainer,
+  CitizensEditEventInfoHeader,
+  CitizensEditEventInfoDescription
+};
