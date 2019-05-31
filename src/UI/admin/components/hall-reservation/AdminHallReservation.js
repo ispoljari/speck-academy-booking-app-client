ebea4972036893a2d6f10114bb0baae18b6bc6f1
Goran Ljaljić <gljaljic@gmail.com>
2019-06-01 01:09:00 +0200

    HTML/CSS with tyled-components almost done. TODO rows/columns in table, events on top of table.

--- src/UI/admin/components/hall-reservation/AdminHallReservation.js
+++ src/UI/admin/components/hall-reservation/AdminHallReservation.js
@@ -1,137 +1,32 @@
 import React from 'react';
 
 import {
-  Container,
-  Krug,
-  Broj,
-  Datum,
-  Oznacite,
-  Podebljano,
-  OdaberiteDatum,
-  OdaberiteVrijeme,
-  Pocetak,
-  Kraj,
-  Unos,
-  Rectangle,
-  Vector,
-  Input,
-  Timeline,
-  Time,
-  Prazno,
-  TimeDivide,
-  TimeDivideFull,
-  TimeDivideWrapper,
-  Zauzeto,
-  Slobodno,
-  Zauzeto1,
-  Legenda1,
-  Legenda2,
-  LegendaBoja,
-  LegendaText
+  MainWrapper,
+  MainTitle,
+  TableAndInfoWrapper,
+  Table,
+  Info,
+  InfoTitle,
+  InfoTime,
+  InfoButton,
+  InfoEraseButton
 } from './style.js';
 
-const a = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 222];
+//const a = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 222];
 
-/*const TimeLine2 = (props) => {
-  return (
-    {a.map(i => {
-      return
-      <div>
-        <Time>8</Time>
-        <TimeDivideFull />
-        <TimeDivide />
-        <TimeDivide />
-        <TimeDivide />
-      </div>
-      })}
-      )
-
-    };
-*/
-
-const TimeLine2 = () => {
-  return (
-    <div>
-      {a.map(i => {
-        return <Time>{i}</Time>;
-      })}
-    </div>
-  );
-};
-
-const TimeLine3 = () => {
-  return (
-    <div>
-      {a.map(i => {
-        return (
-          <TimeDivideWrapper>
-            <TimeDivideFull />
-            <TimeDivide />
-            <TimeDivide />
-            <TimeDivide />
-          </TimeDivideWrapper>
-        );
-      })}
-    </div>
-  );
-};
-
-const CitizensSelectDateTime = () => (
-  <Container>
-    <link
-      rel="stylesheet"
-      media="screen"
-      href="https://fontlibrary.org/face/poppins"
-      type="text/css"
-    />
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="//fonts.googleapis.com/css?family=Lora"
-    />
-    <Krug />
-    <Broj>2</Broj>
-    <Datum>Odaberite datum i vrijeme:</Datum>
-    <Oznacite>
-      Označite slobodan termin na kalendaru za Vaš odabir:{' '}
-      <Podebljano>Konferencijska dvorana RCTP.</Podebljano>
-    </Oznacite>
-    <OdaberiteDatum>ODABERITE DATUM:</OdaberiteDatum>
-    <OdaberiteVrijeme>ODABERITE VRIJEME:</OdaberiteVrijeme>
-    <Pocetak>POČETAK</Pocetak>
-    <Kraj>KRAJ</Kraj>
-    <Unos>
-      <Rectangle>
-        <Input placeholder="DATUM..." type="date" />
-        <Vector />
-      </Rectangle>
-      <Rectangle>
-        <Input placeholder="VRIJEME..." type="text" />
-        <Vector />
-      </Rectangle>
-      <Rectangle>
-        <Input placeholder="VRIJEME..." type="text" />
-        <Vector />
-      </Rectangle>
-    </Unos>
-    <Timeline>
-      <TimeLine2 />
-      <Prazno />
-      <TimeLine3 />
-      <Zauzeto />
-      <Slobodno />
-      <Zauzeto1 />
-    </Timeline>
-    <Legenda1>
-      <LegendaBoja style={{ backgroundColor: '#0f4850' }} />
-      <LegendaText>Zauzeti termini</LegendaText>
-    </Legenda1>
-    <Legenda2>
-      <LegendaBoja style={{ backgroundColor: '#93e9bb' }} />
-      <LegendaText>Odabrani termin</LegendaText>
-    </Legenda2>
-  </Container>
+const ScheduleComponenet = () => (
+  <MainWrapper>
+    <MainTitle>Moj Veeeliki naslov</MainTitle>
+    <TableAndInfoWrapper>
+      <Table />
+      <Info>
+        <InfoTitle />
+        <InfoTime />
+        <InfoButton />
+        <InfoEraseButton />
+      </Info>
+    </TableAndInfoWrapper>
+  </MainWrapper>
 );
 
-export default CitizensSelectDateTime;
-//export TimeLine2, CitizensSelectDateTime;
+export default ScheduleComponenet;
--- src/UI/admin/components/hall-reservation/style.js
+++ src/UI/admin/components/hall-reservation/style.js
@@ -1,337 +1,130 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const MainWrapper = styled.div.attrs({
+  'data-id': 'MainWrapper'
+})`
   position: relative;
   width: 1024px;
-  height: 488px;
+  height: 280px;
   border-radius: 4px;
   border: solid 1px #d9d9d9;
   background-color: #ffffff;
-  margin-left: 208px;
-  margin-top: 32px;
+  margin-left: 0px;
+  margin-top: 0px;
+  background-color: yellow;
 `;
 
-const Krug = styled.div`
-  margin: 16px;
-  width: 152px;
-  height: 152px;
-  opacity: 0.2;
-  border: solid 16px rgba(130, 212, 167, 0.4);
-  border-radius: 50%;
-`;
-
-const Broj = styled.div`
-  position: absolute;
-  top: 20px;
-  left: 16px;
-  width: 76px;
-  height: 145px;
-  opacity: 0.4;
-  font-family: Poppins;
-  font-size: 96px;
-  font-weight: 800;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #82d4a7;
-  margin-left: 38px;
-  text-align: center;
-`;
-
-const Datum = styled.div`
-  position: absolute;
-  top: 32px;
-  left: 130px;
-  width: 576px;
-  height: 40px;
-  font-family: Lora;
-  font-size: 32px;
-  font-weight: bold;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000000;
-`;
-
-const Oznacite = styled.div`
-  position: absolute;
-  top: 94px;
-  left: 130px;
-  width: 764px;
-  height: 24px;
-  font-family: PoppinsRegular;
-  font-size: 14px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000000;
-`;
-
-const Podebljano = styled.span`
-  font-weight: bold;
-`;
-
-const OdaberiteDatum = styled.div`
-  position: absolute;
-  top: 152px;
-  left: 130px;
-  width: 185px;
-  height: 24px;
-  font-family: PoppinsRegular;
-  font-size: 16px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #434343;
-`;
-
-const OdaberiteVrijeme = styled.div`
-  position: absolute;
-  top: 152px;
-  left: 390px;
-  width: 185px;
-  height: 24px;
-  font-family: PoppinsRegular;
-  font-size: 16px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #434343;
-`;
-
-const Pocetak = styled.div`
-  position: absolute;
-  top: 184px;
-  left: 390px;
-  width: 185px;
-  height: 16px;
-  opacity: 0.5;
-  font-family: Poppins-ExtraLight;
-  font-size: 12px;
-  font-weight: 200;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000000;
-`;
-
-const Kraj = styled.div`
-  position: absolute;
-  top: 184px;
-  left: 650px;
-  width: 185px;
-  height: 16px;
-  opacity: 0.5;
-  font-family: Poppins-ExtraLight;
-  font-size: 12px;
-  font-weight: 200;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000000;
-`;
-
-const Unos = styled.div`
-  position: absolute;
-  top: 208px;
-  left: 114px;
-  height: 32px;
-  opacity: 0.5;
-  font-family: Poppins-ExtraLight;
-  font-size: 12px;
-  font-weight: 200;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000000;
-`;
-
-const Rectangle = styled.div`
+const MainTitle = styled.h1.attrs({
+  'data-id': 'MainTitle'
+})`
   position: relative;
-  width: 244px;
+  width: 1024px;
   height: 32px;
-  border-radius: 2px;
-  border: solid 1px rgba(0, 0, 0, 0.15);
-  background-color: #ffffff;
-  margin-left: 16px;
+  background-color: grey;
+  margin-left: 0px;
+  margin-right: 0px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+`;
+
+const TableAndInfoWrapper = styled.div.attrs({
+  'data-id': 'TableAndInfoWrapper'
+})`
+  position: relative;
+  width: 1024px;
+  height: 240px;
+  background-color: red;
+  margin-left: 0px;
+  margin-right: 0px;
+  margin-top: 0px;
+  margin-bottom: 0px;
+`;
+
+const Table = styled.table.attrs({
+  'data-id': 'Table'
+})`
+  position: relative;
+  width: 776px;
+  height: 208px;
+  background-color: black;
+  margin-left: 24px;
+  margin-right: 0px;
+  margin-top: 16px;
+  margin-bottom: 0px;
   display: inline-block;
 `;
 
-const Vector = styled.div`
+const Info = styled.div.attrs({
+  'data-id': 'Info'
+})`
   position: absolute;
-  top: 13px;
-  left: 220px;
-  width: 8px;
-  height: 4px;
-  border-left: 4px solid transparent;
-  border-right: 4px solid transparent;
-  border-top: 4px solid #555;
+  width: 200px;
+  height: 208px;
+  background-color: pink;
+  margin-left: 17px;
+  margin-right: 7px;
+  margin-top: 16px;
+  margin-bottom: 0px;
+  display: inline-block;
 `;
 
-const Input = styled.input`
-  position: absolute;
-  width: 244px;
-  height: 32px;
-  content: attr(placeholder) !important;
-`;
-
-const Timeline = styled.div`
-  position: absolute;
-  top: 272px;
-  left: 130px;
-  width: 750px;
+const InfoTitle = styled.h1.attrs({
+  'data-id': 'InfoTitle'
+})`
+  position: relative;
+  width: 184px;
   height: 64px;
+  background-color: grey;
+  margin-left: 8px;
+  margin-right: 8px;
+  margin-top: 8px;
 `;
 
-const Time = styled.div`
-  width: 50px;
+const InfoTime = styled.h1.attrs({
+  'data-id': 'InfoTime'
+})`
+  position: relative;
+  width: 184px;
   height: 16px;
-  font-family: Poppins;
-  font-size: 12px;
-  font-weight: 200;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: rgba(0, 0, 0, 0.5);
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-  display: inline-block;
-  text-align: center;
+  background-color: grey;
+  margin-left: 8px;
+  margin-right: 8px;
+  margin-top: 16px;
+  margin-bottom: 0px;
 `;
 
-const Prazno = styled.div`
-  width: 25px;
-  height: 48px;
-  display: inline-block;
-`;
-
-const TimeDivide = styled.div`
-  width: 12.5px;
-  height: 48px;
-  border-left: 0.5px dashed rgba(0, 0, 0, 0.1);
-  display: inline-block;
-`;
-
-const TimeDivideFull = styled.div`
-  width: 12.5px;
-  height: 48px;
-  border-left: 0.5px solid rgba(0, 0, 0, 0.2);
-  display: inline-block;
-`;
-
-const TimeDivideWrapper = styled.div`
-  width: 52px;
-  height: 48px;
-  display: inline-block;
-`;
-
-const Zauzeto = styled.div`
-  position: absolute;
-  top: 24px;
-  left: 76px;
-  width: 212px;
+const InfoButton = styled.button.attrs({
+  'data-id': 'InfoButton'
+})`
+  position: relative;
+  width: 32px;
   height: 32px;
-  border-radius: 2px;
-  background-color: #0f4850;
+  background-color: orange;
+  margin-left: 8px;
+  margin-top: 8px;
+  margin-bottom: 0px;
 `;
 
-const Slobodno = styled.div`
-  position: absolute;
-  top: 24px;
-  left: 312px;
-  width: 150px;
-  height: 32px;
-  border-radius: 2px;
-  background-color: #93e9bb;
-`;
-
-const Zauzeto1 = styled.div`
-  position: absolute;
-  top: 24px;
-  left: 550px;
-  width: 88px;
-  height: 32px;
-  border-radius: 2px;
-  background-color: #0f4850;
-`;
-
-const Legenda1 = styled.div`
-  position: absolute;
-  top: 368px;
-  left: 130px;
-  width: 200px;
+const InfoEraseButton = styled.button.attrs({
+  'data-id': 'InfoEraseButton'
+})`
+  position: relative;
+  width: 184px;
   height: 40px;
-  display: inline-block;
-`;
-
-const Legenda2 = styled.div`
-  position: absolute;
-  top: 416px;
-  left: 130px;
-  width: 200px;
-  height: 40px;
-`;
-
-const LegendaBoja = styled.div`
-  width: 40px;
-  height: 40px;
-  border-radius: 2px;
-  display: inline-block;
-`;
-
-const LegendaText = styled.div`
-  position: absolute;
-  margin: 8px;
-  margin-top: 13px;
-  font-family: Poppins-Regular;
-  font-size: 14px;
-  font-weight: normal;
-  font-style: normal;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  color: #000000;
-  display: inline-block;
-  vertical-align: middle;
+  background-color: purple;
+  margin-left: 8px;
+  margin-top: 8px;
+  margin-bottom: 0px;
 `;
 
 export {
-  Container,
-  Krug,
-  Broj,
-  Datum,
-  Oznacite,
-  Podebljano,
-  OdaberiteDatum,
-  OdaberiteVrijeme,
-  Pocetak,
-  Kraj,
-  Unos,
-  Rectangle,
-  Vector,
-  Input,
-  Timeline,
-  Time,
-  Prazno,
-  TimeDivide,
-  TimeDivideFull,
-  TimeDivideWrapper,
-  Zauzeto,
-  Slobodno,
-  Zauzeto1,
-  Legenda1,
-  Legenda2,
-  LegendaBoja,
-  LegendaText
+  MainWrapper,
+  MainTitle,
+  TableAndInfoWrapper,
+  Table,
+  Info,
+  InfoTitle,
+  InfoTime,
+  InfoButton,
+  InfoEraseButton
 };
