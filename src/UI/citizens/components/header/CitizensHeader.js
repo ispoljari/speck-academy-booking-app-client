49d2ef8115ab011c647b699fbf3f0266ba16abf3
abiskup <andrija.biskup@gmail.com>
2019-05-31 19:33:06 +0200

    Add citzen-header, necessary refinements

--- src/UI/citizens/components/header/CitizensHeader.js
+++ src/UI/citizens/components/header/CitizensHeader.js
@@ -2,14 +2,52 @@ import React from 'react';
 
 import {
   HeaderContainer,
-  HeaderTitle
+  HeaderTitle,
+  RezervacijaDvorana,
+  RezervacijaDvoranaMalo,
+  Okvir,
+  Okvir2,
+  Naslov,
+  Text,
+  Okvir3,
+  HeaderImage
 } from './CitizensHeaderStyle';
+import headerImage from '../../../../images/headerimage.jpg';
 
 const CitizensHeader = () => (
   <HeaderContainer>
-    <HeaderTitle>
-      Speck Academy Booking App!
-    </HeaderTitle>
+    <Okvir>
+      <button type="button">ADMIN LOGIN</button>
+    </Okvir>
+    <HeaderImage src={headerImage} />
+    <RezervacijaDvorana>
+      Rezervacija Dvorana
+      <br />
+      <RezervacijaDvoranaMalo>u gradu Križevcima</RezervacijaDvoranaMalo>
+    </RezervacijaDvorana>
+    <Okvir2>
+      <Naslov>
+        Kontaktirajte nas!
+        <br />
+        <Text>
+          Na ovim stranicama grada Križevaca možete odabrati dvorane i njihove
+          termine koji odgovaraju Vašim događanima i eventovima.
+        </Text>
+      </Naslov>
+    </Okvir2>
+    <Okvir3>
+      <Naslov>
+        Dobrodošli, građani
+        <br />
+        <Text>
+          Ako imate pitanja ili ste naišli na nejasnoće,
+          <br />
+          git kontaktirajte nas putem e-maila ili poziva.
+        </Text>
+      </Naslov>
+    </Okvir3>
+
+    <HeaderTitle />
   </HeaderContainer>
 );
 
--- src/UI/citizens/components/header/CitizensHeaderStyle.js
+++ src/UI/citizens/components/header/CitizensHeaderStyle.js
@@ -1,12 +1,160 @@
 import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
-  width: 100%;
+  width: 1440px;
   padding: 20px;
 `;
 
 const HeaderTitle = styled.h1`
-  color: red;
+  width: 576px;
+  height: 40px;
+  font-family: Lora;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
 `;
 
-export { HeaderContainer, HeaderTitle };
+const HeaderButton = styled.div`
+  width: 108px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #0f4850;
+`;
+
+const HeaderImage = styled.img`
+width: 100%;
+height: 336px;
+top center / 200px 200px;
+`;
+
+const RezervacijaDvorana = styled.div`
+  left: 208px;
+  top: 145.6px
+  position: absolute;
+  width: 1024px;
+  height: 89.6px;
+  text-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
+  font-family: Lora;
+  font-size: 48px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ffffff;
+  `;
+
+const RezervacijaDvoranaMalo = styled.div`
+  width: 1024px;
+  height: 89.6px;
+  text-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
+  font-family: Lora;
+  font-size: 20px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ffffff;
+`;
+const Okvir = styled.div`
+  left: 1092px;
+  top: 32px
+  position: absolute;
+  width: 132px;
+  height: 40px;
+  border-radius: 4px;
+  border: solid 1px #0f4850;
+  background-color: #ffffff;
+  `;
+
+const Gumb = styled.button`
+  width: 108px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #0f4850;
+`;
+
+const Naslov = styled.div`
+  position: relative;
+  width: 576px;
+  height: 40px;
+  font-family: Lora;
+  font-size: 32px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+`;
+
+const Text = styled.div`
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #000000;
+`;
+
+const Okvir2 = styled.div`
+  left: 208px;
+  top: 288px
+  position: absolute;
+  width: 634px;
+  height: 208px;    
+  border-radius: 4px;
+  border: solid 1px #d9d9d9;
+  background-color: #ffffff;
+  padding:10px;
+  margin-left: 20px;
+  margin-top:20px;
+  `;
+
+const Okvir3 = styled.div`
+  left: 858px;
+  top: 288px
+  position: absolute;
+  width: 374px;
+  height: 208px;
+  border-radius: 4px;
+  border: solid 1px #d9d9d9;
+  background-color: #ffffff;
+  padding:10px;
+  margin-left: 20px;
+  margin-top:20px;
+  `;
+
+export {
+  HeaderContainer,
+  Okvir3,
+  Text,
+  Okvir2,
+  Naslov,
+  Okvir,
+  Gumb,
+  HeaderTitle,
+  RezervacijaDvoranaMalo,
+  RezervacijaDvorana,
+  HeaderImage,
+  HeaderButton
+};
