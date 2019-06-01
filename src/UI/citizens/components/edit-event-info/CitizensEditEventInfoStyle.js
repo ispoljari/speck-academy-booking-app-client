a1f46b80015c917dac5ae4f9a3e0ce55a10cf028
Ivica Sirjan <isirjan@gmail.com>
2019-06-01 02:57:14 +0200

    import React from 'react';
    import {
      MainContainer,
      MainHeader,
      MainDescription,
      FormContainer,
      FormHeader,
      FormInput,
      FormInputText,
      FormInlineLabel,
      FormInlineInput,
    } from './CitizensEditEventInfoStyle';
    
    const CitizensEditEventInfo = () => (
      <MainContainer>
        <MainHeader>Podaci o događaju i Vama</MainHeader>
        <MainDescription>
          Recite nam ime svog događaja i pobliže ga objasnite. Nemojte zaboraviti
          ostaviti svoje kontakte podatke kako bismo Vas mogli kontaktirati u vezi
          dodatnih detalja i potvrđivanja rezervacije.
        </MainDescription>
        <FormContainer>
          <FormHeader>Naziv događaja:</FormHeader>
          <FormInput />
          <FormHeader>Opis događaja:</FormHeader>
          <FormInputText value="Opišite svoj događaj..." />
          <FormHeader>Vaši kontakt podaci:</FormHeader>
          <FormInlineInput>
            <FormInlineLabel>Ime i prezime</FormInlineLabel>
            <FormInput />
          </FormInlineInput>
          <FormInlineInput>
            <FormInlineLabel>Organizacija (neobavezno)</FormInlineLabel>
            <FormInput />
          </FormInlineInput>
          <FormInlineInput>
            <FormInlineLabel>E-mail adresa</FormInlineLabel>
            <FormInput />
          </FormInlineInput>
          <FormInlineInput>
            <FormInlineLabel>Broj telefona/Mobitela</FormInlineLabel>
              <FormInput />
          </FormInlineInput>
        </FormContainer>
      </MainContainer>
    );
    
    export default CitizensEditEventInfo;

--- src/UI/citizens/components/edit-event-info/CitizensEditEventInfoStyle.js
+++ src/UI/citizens/components/edit-event-info/CitizensEditEventInfoStyle.js
@@ -12,18 +12,21 @@ const MainContainer = styled.div`
 `;
 
 const MainHeader = styled.h1`
+  margin-top: 32px;
+  margin-bottom: 16px;
   font-family: Lora;
   font-size: 32px;
-  font-weight: bold;
   color: #000000;
 `;
 
 const MainDescription = styled.p`
   font-family: Poppins;
   font-size: 14px;
-  font-weight: normal;
   color: #000000;
+  paddng-bottom: 16px;
 `;
+
+const FormContainer = styled.form``;
 
 const FormHeader = styled.h5`
   text-transform: uppercase;
@@ -31,6 +34,56 @@ const FormHeader = styled.h5`
   font-size: 16px;
   font-weight: normal;
   color: #434343;
+  padding-top: 16px;
 `;
 
-export { MainContainer, MainHeader, MainDescription, FormHeader };
+const FormInput = styled.input`
+  width: 100%;
+  height: 32px;
+  border-radius: 2px;
+  border: solid 1px rgba(0, 0, 0, 0.15);
+  background-color: #ffffff;
+  margin-top: 8px;
+`;
+
+const FormInputText = styled.textarea`
+  width: 100%;
+  height: 136px;
+  padding-left: 8px;
+  padding-top: 6px;
+  padding-right: 8px;
+  padding-bottom: 6px;
+  margin-top: 8px;
+  margin-bottom: 4px;
+  border-radius: 2px;
+  border: solid 1px rgba(0, 0, 0, 0.15);
+  background-color: #ffffff;
+`;
+
+const FormInlineLabel = styled.h6`
+  text-transform: uppercase;
+  opacity: 0.5;
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 200;
+  color: #000000;
+  margin-top: 8px;
+`;
+
+const FormInlineInput = styled.span`
+  display: inline-block;
+  width: 374px;
+  margin-right: 7px;
+`;
+
+export {
+  MainContainer,
+  MainHeader,
+  MainDescription,
+  FormContainer,
+  FormHeader,
+  FormInput,
+  FormInputText,
+  FormInlineLabel,
+  FormInlineInput
+};
