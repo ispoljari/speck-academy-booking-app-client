9a1aa461a6bede862fd163b12c568a61b90d716b
Goran Ljaljić <gljaljic@gmail.com>
2019-06-03 00:04:38 +0200

    HTML almost done.TODO-Pop-up divs,Dynamic div adding

--- src/UI/admin/components/hall-reservation/AdminHallReservation.js
+++ src/UI/admin/components/hall-reservation/AdminHallReservation.js
@@ -2,28 +2,180 @@ import React from 'react';
 
 import {
   MainWrapper,
+  MainTitleWrapper,
   MainTitle,
   TableAndInfoWrapper,
-  Table,
+  TableWrapper,
+  HoursDivRow,
+  HourDiv,
+  Line,
+  OneDayDivRow,
+  ReservationDynamicDiv,
+  //Table,
+  //TableData,
   Info,
   InfoTitle,
   InfoTime,
   InfoButton,
-  InfoEraseButton
+  InfoEraseButton,
+  InfoEraseButtonText
 } from './style.js';
 
 //const a = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 222];
 
 const ScheduleComponenet = () => (
   <MainWrapper>
-    <MainTitle>Moj Veeeliki naslov</MainTitle>
+    <MainTitleWrapper>
+      <MainTitle>Moj Veeeliki naslov</MainTitle>
+    </MainTitleWrapper>
     <TableAndInfoWrapper>
-      <Table />
+      <TableWrapper>
+        <HoursDivRow>
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+          <HourDiv />
+        </HoursDivRow>
+        <Line />
+        <OneDayDivRow>
+          PON
+          <ReservationDynamicDiv> </ReservationDynamicDiv>
+          <ReservationDynamicDiv> </ReservationDynamicDiv>
+        </OneDayDivRow>
+        <OneDayDivRow>
+          UTO
+          <ReservationDynamicDiv> </ReservationDynamicDiv>
+          <ReservationDynamicDiv> </ReservationDynamicDiv>
+          <ReservationDynamicDiv> </ReservationDynamicDiv>
+        </OneDayDivRow>
+        <OneDayDivRow>
+          SRI
+          <ReservationDynamicDiv> </ReservationDynamicDiv>
+        </OneDayDivRow>
+        <OneDayDivRow>ČET</OneDayDivRow>
+        <OneDayDivRow>PET</OneDayDivRow>
+        <OneDayDivRow>SUB</OneDayDivRow>
+        <OneDayDivRow>NED</OneDayDivRow>
+      </TableWrapper>
+
+      {/*<Table />*/}
+      {/*
+      <Table id="leaderboard_view" >
+      
+      <tbody>
+        <tr>
+          <th></th>
+          <th>8</th>
+          <th>9</th>
+          <th>10</th>
+          <th>11</th>
+          <th>12</th>
+          <th>13</th>
+          <th>14</th>
+          <th>15</th>
+          <th>16</th>
+          <th>17</th>
+          <th>18</th>
+          <th>19</th>
+          <th>20</th>
+        </tr>
+        <tr>
+          <th>PON</th>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+        </tr>
+        <tr>
+          <th>UTO</th>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+        </tr>
+        <tr>
+          <th>SRI</th>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+        </tr>
+        <tr>
+          <th>ČET</th>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+        </tr>
+        <tr>
+          <th>PET</th>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+          <TableData></TableData><TableData></TableData><TableData></TableData><TableData></TableData>
+        </tr>
+      </tbody>
+      </Table>*/}
+
       <Info>
-        <InfoTitle />
-        <InfoTime />
+        <InfoTitle>
+          Bad to the minton, Bad to the minton, Bad to the minton
+        </InfoTitle>
+        <InfoTime>23.11.2018., 12:15 - 15:45</InfoTime>
         <InfoButton />
-        <InfoEraseButton />
+        <InfoEraseButton>
+          <InfoEraseButtonText>IZBRIŠI</InfoEraseButtonText>
+        </InfoEraseButton>
       </Info>
     </TableAndInfoWrapper>
   </MainWrapper>
--- src/UI/admin/components/hall-reservation/style.js
+++ src/UI/admin/components/hall-reservation/style.js
@@ -7,20 +7,39 @@ const MainWrapper = styled.div.attrs({
   width: 1024px;
   height: 280px;
   border-radius: 4px;
-  border: solid 1px #d9d9d9;
+  border: solid 1px #229c7f;
   background-color: #ffffff;
+
   margin-left: 0px;
   margin-top: 0px;
-  background-color: yellow;
+`;
+
+const MainTitleWrapper = styled.div.attrs({
+  'data-id': 'MainTitleWrapper'
+})`
+  position: relative;
+  width: 1024px;
+  height: 40px;
+  border-radius: 3px;
+  background-color: #229c7f;
 `;
 
 const MainTitle = styled.h1.attrs({
   'data-id': 'MainTitle'
 })`
-  position: relative;
   width: 1024px;
   height: 32px;
-  background-color: grey;
+  font-family: Lora;
+  font-size: 24px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #ffffff;
+
+  text-align: center;
+  position: relative;
   margin-left: 0px;
   margin-right: 0px;
   margin-top: 4px;
@@ -33,20 +52,20 @@ const TableAndInfoWrapper = styled.div.attrs({
   position: relative;
   width: 1024px;
   height: 240px;
-  background-color: red;
   margin-left: 0px;
   margin-right: 0px;
   margin-top: 0px;
   margin-bottom: 0px;
 `;
 
-const Table = styled.table.attrs({
-  'data-id': 'Table'
+const TableWrapper = styled.div.attrs({
+  'data-id': 'TableWrapper'
 })`
   position: relative;
   width: 776px;
   height: 208px;
-  background-color: black;
+
+  background-color: white;
   margin-left: 24px;
   margin-right: 0px;
   margin-top: 16px;
@@ -54,13 +73,119 @@ const Table = styled.table.attrs({
   display: inline-block;
 `;
 
+const HoursDivRow = styled.div.attrs({
+  'data-id': 'HoursDivRow'
+})`
+  width: 708px;
+  height: 16px;
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 200;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: rgba(0, 0, 0, 0.5);
+
+  margin-left: 68px;
+  margin-right: 0px;
+  margin-top: 24px;
+  margin-bottom: 0px;
+  background-color: blue;
+`;
+
+//TODO position this div(s) with repeat
+const HourDiv = styled.div.attrs({
+  'data-id': 'HourDiv'
+})`
+  width: 50px;
+  height: 16px;
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 200;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: rgba(0, 0, 0, 0.5);
+
+  display: inline-block;
+  position: relative;
+
+  border: 0.2px solid black;
+  background-color: yellow;
+`;
+
+const Line = styled.div.attrs({
+  'data-id': 'Line'
+})`
+  width: 776px;
+  height: 0px;
+  border: solid 1px rgba(0, 0, 0, 0.15);
+`;
+
+const OneDayDivRow = styled.div.attrs({
+  'data-id': 'OneDayDivRow'
+})`
+  width: 747px;
+  height: 16px;
+  border-radius: 2px;
+  background-color: #f0f0f0;
+
+  margin-left: 29px;
+  margin-right: 0px;
+  margin-top: 5px;
+  margin-bottom: 0px;
+`;
+
+//TODO position this div(s) dynamicaly
+//width and margin-left and opacitiy
+const ReservationDynamicDiv = styled.div.attrs({
+  'data-id': 'ReservationDynamicDiv'
+})`
+  width: 175px;
+  height: 14px;
+  border-radius: 2px;
+  border: solid 1px #ffffff;
+  background-color: #93e9bb;
+
+  display: inline-block;
+  margin-left: 29px;
+`;
+
+/*
+const Table = styled.table.attrs({
+  'data-id': 'Table'
+})`
+  position: relative;
+  width: 776px;
+  height: 208px;
+  background-color: brown;
+  margin-left: 24px;
+  margin-right: 0px;
+  margin-top: 16px;
+  margin-bottom: 0px;
+  display: inline-block;
+`;
+*/
+
+/*
+const TableData = styled.td.attrs({
+  'data-id': 'TableData'
+})`
+  border: 1px solid black;
+`;
+*/
+
 const Info = styled.div.attrs({
   'data-id': 'Info'
 })`
-  position: absolute;
   width: 200px;
   height: 208px;
-  background-color: pink;
+  border-radius: 2px;
+  background-color: #edf5f1;
+
+  position: absolute;
   margin-left: 17px;
   margin-right: 7px;
   margin-top: 16px;
@@ -71,10 +196,18 @@ const Info = styled.div.attrs({
 const InfoTitle = styled.h1.attrs({
   'data-id': 'InfoTitle'
 })`
-  position: relative;
   width: 184px;
   height: 64px;
-  background-color: grey;
+  font-family: Lora;
+  font-size: 18px;
+  font-weight: bold;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #229c7f;
+
+  position: relative;
   margin-left: 8px;
   margin-right: 8px;
   margin-top: 8px;
@@ -83,10 +216,18 @@ const InfoTitle = styled.h1.attrs({
 const InfoTime = styled.h1.attrs({
   'data-id': 'InfoTime'
 })`
-  position: relative;
   width: 184px;
   height: 16px;
-  background-color: grey;
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: normal;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.05;
+  letter-spacing: normal;
+  color: #000000;
+
+  position: relative;
   margin-left: 8px;
   margin-right: 8px;
   margin-top: 16px;
@@ -96,10 +237,13 @@ const InfoTime = styled.h1.attrs({
 const InfoButton = styled.button.attrs({
   'data-id': 'InfoButton'
 })`
-  position: relative;
   width: 32px;
   height: 32px;
-  background-color: orange;
+  border-radius: 2px;
+  border: solid 1px #93e9bb;
+  background-color: #ffffff;
+
+  position: relative;
   margin-left: 8px;
   margin-top: 8px;
   margin-bottom: 0px;
@@ -108,23 +252,56 @@ const InfoButton = styled.button.attrs({
 const InfoEraseButton = styled.button.attrs({
   'data-id': 'InfoEraseButton'
 })`
-  position: relative;
   width: 184px;
   height: 40px;
-  background-color: purple;
+  border-radius: 4px;
+  border: solid 1px #0f4951;
+  background-color: #ffffff;
+
+  position: relative;
   margin-left: 8px;
   margin-top: 8px;
   margin-bottom: 0px;
 `;
 
+const InfoEraseButtonText = styled.p.attrs({
+  'data-id': 'InfoEraseButtonText'
+})`
+  width: 136px;
+  height: 24px;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #0f4951;
+
+  position: relative;
+  margin-left: 12px;
+  margin-right: 12px;
+  margin-top: 10px;
+  margin-bottom: 6px;
+`;
+
 export {
   MainWrapper,
+  MainTitleWrapper,
   MainTitle,
   TableAndInfoWrapper,
-  Table,
+  TableWrapper,
+  HoursDivRow,
+  HourDiv,
+  Line,
+  OneDayDivRow,
+  ReservationDynamicDiv,
+  //Table,
+  //TableData,
   Info,
   InfoTitle,
   InfoTime,
   InfoButton,
-  InfoEraseButton
+  InfoEraseButton,
+  InfoEraseButtonText
 };
