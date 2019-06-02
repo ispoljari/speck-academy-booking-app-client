1842b201279421eae35e82527935a344057b160e
Igor Sustic <igor.sustic@gmail.com>
2019-06-03 00:14:15 +0200

    Add styling to AddHallComponent, add image in src/images folder for AddHallComponent

--- src/UI/admin/components/available-hall/AddHall.js
+++ src/UI/admin/components/available-hall/AddHall.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import Plus from '../../../../images/PlusSign.svg';
 
 const Wrapper = styled.div`
   box-sizing: border-box;
@@ -14,6 +15,43 @@ const Wrapper = styled.div`
   background-color: #ffffff;
 `;
 
-const AddHallComponent = () => <Wrapper />;
+const PlusSign = styled.img`
+  margin-top: 92px;
+  width: 104px;
+  height: 104px;
+`;
+
+const ButtonAdd = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  box-sizing: border-box;
+  margin-top: 100px;
+  width: 206px;
+  height: 40px;
+  border-radius: 4px;
+  border: solid 1px #93e9bb;
+  background-color: #ffffff;
+
+   {
+    /* stil za tekst unutar buttona */
+  }
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 500;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #93e9bb;
+`;
+
+const AddHallComponent = () => (
+  <Wrapper>
+    <PlusSign src={Plus} />
+    <ButtonAdd>DODAJ NOVU DVORANU</ButtonAdd>
+  </Wrapper>
+);
 
 export default AddHallComponent;
