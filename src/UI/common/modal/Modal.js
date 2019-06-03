9020f86787cdd7379a60f57b1c179a194e82a77b
ispoljari <ivanspoljaric22@gmail.com>
2019-06-03 13:15:10 +0200

    Create a class Modal component with basic state

--- src/UI/common/modal/Modal.js
+++ src/UI/common/modal/Modal.js
@@ -1,1 +1,15 @@
 import React, { Component } from 'react';
+
+class Modal extends Component {
+  state = {
+    visible: false
+  };
+
+  render() {
+    return (
+      <div>
+        I'm a dumb modal component.
+      </div>
+    )
+  }
+}
