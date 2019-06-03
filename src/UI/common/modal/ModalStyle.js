e994ac287c2dfb38ef390cd41efc847aa4b7bcbb
ispoljari <ivanspoljaric22@gmail.com>
2019-06-03 13:48:00 +0200

    Create Box comp with a basic style API

--- src/UI/common/modal/ModalStyle.js
+++ src/UI/common/modal/ModalStyle.js
@@ -4,6 +4,25 @@ const ModalContainer = styled.div`
   display: ${props => props.visible ? 'block' : 'none'};
 `;
 
+const Box = styled.div`
+  display: block;
+
+  ${props => {
+    let sumCSS = ``;
+
+    props.pos ? sumCSS += `position: ${props.pos}` : '';
+    props.top ? sumCSS += `top: ${props.top}` : '';
+    props.left ? sumCSS += `left: ${props.left}` : '';
+    props.width ? sumCSS += `width: ${props.wd}` : '';
+    props.maxWidth ? sumCSS += `max-width: ${props.maxWd}` : '';
+    props.height ? sumCSS += `height: ${props.hg}` : '';
+    props.maxHeight ? sumCSS += `max-height: ${props.maxHg}` : '';
+    props.transform ? sumCSS += `transform: ${props.transf}` : '';
+
+    return sumCSS;
+  }}
+`;
+
 export {
   ModalContainer
 };
