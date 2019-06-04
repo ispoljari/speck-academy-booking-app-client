90bddbf2cfc55ded467042dd95023f0159ee9d31
ispoljari <ivanspoljaric22@gmail.com>
2019-06-04 08:48:31 +0200

    Remove css import from styled components

--- src/UI/common/modal/ModalStyle.js
+++ src/UI/common/modal/ModalStyle.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 const ModalContainer = styled.div`
   display: ${props => (props.visible ? 'block' : 'none')};
