22e429623cba4acc8dedcc80efdedb8bb5c0920e
ispoljari <ivanspoljaric22@gmail.com>
2019-06-12 16:29:48 +0200

    Transform CitizensHeader function into class component

--- src/UI/citizens/components/header/CitizensHeader.js
+++ src/UI/citizens/components/header/CitizensHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {
   HeaderContainer,
@@ -20,49 +20,53 @@ import {
 
 import headerImage from '../../../../images/headerimage.jpg';
 
-const CitizensHeader = () => (
-  <HeaderContainer>
-    <Header imgSrc={headerImage}>
-      {/* <HeaderImage src={headerImage} />*/}
+class CitizensHeader extends Component {
+  render() {
+    return (
+      <HeaderContainer>
+        <Header imgSrc={headerImage}>
+          {/* <HeaderImage src={headerImage} />*/}
 
-      <Container>
-        <Admin>ADMIN LOGIN</Admin>
+          <Container>
+            <Admin>ADMIN LOGIN</Admin>
 
-        <HeaderTitle>
-          REZERVACIJA DVORANA
-          <br />
-          <HeaderTitleSmall>u gradu Križevcima</HeaderTitleSmall>
-        </HeaderTitle>
-
-        <Container1>
-          <ContactInformation1>
-            <Title>Dobrodošli, građani</Title>
-            <br />
-            <Text>
-              Na ovim stranicama grada Križevaca možete odabrati dvorane i
-              njihove termine koji odgovaraju Vašim događanima i eventovima.
-            </Text>
-          </ContactInformation1>
-          <ContactInformation>
-            <Title>Kontaktirajte nas!</Title>
-            <br />
-            <Text1>
-              Ako imate pitanja ili ste naišli na nejasnoće,
+            <HeaderTitle>
+              REZERVACIJA DVORANA
               <br />
-              kontaktirajte nas putem e-maila ili poziva.
-            </Text1>
-            <ContactBox>
-              <Mail>E-MAIL</Mail>
-              <Adresa>info@grad-krizevci.hr</Adresa>
-              <br />
-              <Mail>MOBITEL</Mail>
-              <Adresa>099 / 12 34 5678</Adresa>
-            </ContactBox>
-          </ContactInformation>
-        </Container1>
-      </Container>
-    </Header>
-  </HeaderContainer>
-);
+              <HeaderTitleSmall>u gradu Križevcima</HeaderTitleSmall>
+            </HeaderTitle>
+
+            <Container1>
+              <ContactInformation1>
+                <Title>Dobrodošli, građani</Title>
+                <br />
+                <Text>
+                  Na ovim stranicama grada Križevaca možete odabrati dvorane i
+                  njihove termine koji odgovaraju Vašim događanima i eventovima.
+                </Text>
+              </ContactInformation1>
+              <ContactInformation>
+                <Title>Kontaktirajte nas!</Title>
+                <br />
+                <Text1>
+                  Ako imate pitanja ili ste naišli na nejasnoće,
+                  <br />
+                  kontaktirajte nas putem e-maila ili poziva.
+                </Text1>
+                <ContactBox>
+                  <Mail>E-MAIL</Mail>
+                  <Adresa>info@grad-krizevci.hr</Adresa>
+                  <br />
+                  <Mail>MOBITEL</Mail>
+                  <Adresa>099 / 12 34 5678</Adresa>
+                </ContactBox>
+              </ContactInformation>
+            </Container1>
+          </Container>
+        </Header>
+      </HeaderContainer>
+    );
+  }
+}
 
 export default CitizensHeader;
