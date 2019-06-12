d8e6299aba1d213ed3e191f4d00b1d904c4b1a3d
antonio.hadrovic <hadrovic1@gmail.com>
2019-06-12 20:36:03 +0200

    Add Favicon

--- src/UI/citizens/components/header/CitizensHeader.js
+++ src/UI/citizens/components/header/CitizensHeader.js
@@ -18,50 +18,55 @@ import {
   Container1
 } from './CitizensHeaderStyle';
 
-import headerImage from '../../../../images/headerimage.jpg';
+import headerImage from '../../../../images/header-image@2x.png';
 
 const CitizensHeader = () => (
   <HeaderContainer>
-    <Header imgSrc={headerImage}>
-      {/* <HeaderImage src={headerImage} />*/}
+    <Header imgSrc={headerImage}> /</Header>
+    <Container>
+      <Admin>ADMIN LOGIN</Admin>
 
-      <Container>
-        <Admin>ADMIN LOGIN</Admin>
+      <HeaderTitle>
+        REZERVACIJA DVORANA
+        <br />
+        <HeaderTitleSmall>u gradu Križevcima</HeaderTitleSmall>
+      </HeaderTitle>
 
-        <HeaderTitle>
-          REZERVACIJA DVORANA
+      <Container1>
+        <ContactInformation1>
+          <Title>Dobrodošli, građani</Title>
           <br />
-          <HeaderTitleSmall>u gradu Križevcima</HeaderTitleSmall>
-        </HeaderTitle>
-
-        <Container1>
-          <ContactInformation1>
-            <Title>Dobrodošli, građani</Title>
+          <Text>
+            Na ovim stranicama grada Križevaca možete odabrati dvorane i njihove
+            termine koji odgovaraju Vašim događanima i eventovima.
+          </Text>
+        </ContactInformation1>
+        <ContactInformation>
+          <Title>Kontaktirajte nas!</Title>
+          <br />
+          <Text1>
+            Ako imate pitanja ili ste naišli na nejasnoće,
             <br />
-            <Text>
-              Na ovim stranicama grada Križevaca možete odabrati dvorane i
-              njihove termine koji odgovaraju Vašim događanima i eventovima.
-            </Text>
-          </ContactInformation1>
-          <ContactInformation>
-            <Title>Kontaktirajte nas!</Title>
+            kontaktirajte nas putem e-maila ili poziva.
+          </Text1>
+          <ContactBox>
+            <Mail>E-MAIL</Mail>
+            <Adresa>
+              <a
+                href="mailto:info@grad-krizevci.hr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                info@grad-krizevci.hr
+              </a>
+            </Adresa>
             <br />
-            <Text1>
-              Ako imate pitanja ili ste naišli na nejasnoće,
-              <br />
-              kontaktirajte nas putem e-maila ili poziva.
-            </Text1>
-            <ContactBox>
-              <Mail>E-MAIL</Mail>
-              <Adresa>info@grad-krizevci.hr</Adresa>
-              <br />
-              <Mail>MOBITEL</Mail>
-              <Adresa>099 / 12 34 5678</Adresa>
-            </ContactBox>
-          </ContactInformation>
-        </Container1>
-      </Container>
-    </Header>
+            <Mail>MOBITEL</Mail>
+            <Adresa>099 / 12 34 5678</Adresa>
+          </ContactBox>
+        </ContactInformation>
+      </Container1>
+    </Container>
   </HeaderContainer>
 );
 
--- src/UI/citizens/components/header/CitizensHeaderStyle.js
+++ src/UI/citizens/components/header/CitizensHeaderStyle.js
@@ -1,17 +1,21 @@
 import styled from 'styled-components';
 
 const HeaderContainer = styled.header`
+  position: relative;
   width: 1440px;
-  padding: 32px;
-  margin-bottom: 200px;
+  //padding: 32px;
+  margin-bottom: 0px;
+  color: #fff;
 `;
 
 const Header = styled.div`
+  position: absolute;
   width: 1440px;
   height: 336px;
   background-position: center;
   background-size: cover;
   background-image: url(${props => props.imgSrc});
+  z-index: 1;
 `;
 
 const HeaderTitle = styled.div`
@@ -19,27 +23,31 @@ const HeaderTitle = styled.div`
   width: 100%;
   height: 90px;
   text-shadow: 0 0 8px rgba(0, 0, 0, 0.6);
-  font-family: Lora;
+  font-family: Lora, serif;
   font-size: 48px;
-  font-weight: bold;
-  color: #ffffff;
+  font-weight: 700;
+  color: #fff;
+  z-index: 2;
 `;
 
 const HeaderTitleSmall = styled(HeaderTitle)`
   margin-top: 0px;
   font-size: 20px;
+  z-index: 2;
 `;
 
 const Admin = styled.button`
+  margin-top: 32px;
   margin-left: 884px;
-  font-family: Poppins;
+  font-family: Poppins, sans-serif;
   font-weight: 500;
   color: #0f4850;
   width: 132px;
   height: 40px;
   border-radius: 4px;
   border: solid 1px #0f4850;
-  background-color: #ffffff;
+  background-color: #fff;
+  z-index: 2;
 `;
 
 const Title = styled.div`
@@ -47,19 +55,21 @@ const Title = styled.div`
   margin-bottom: 0px;
   width: 576px;
   height: 40px;
-  font-family: Lora;
+  font-family: Lora, serif;
   font-size: 32px;
   font-weight: bold;
   color: #000000;
+  z-index: 2;
 `;
 
 const Text = styled.div`
   margin: 32px;
   margin-top: 16px;
-  font-family: Poppins;
+  font-family: Poppins, sans-serif;
   font-size: 14px;
   font-weight: normal;
-  color: #000000;
+  color: #000;
+  z-index: 2;
 `;
 
 const Text1 = styled.div`
@@ -67,7 +77,8 @@ const Text1 = styled.div`
   font-family: Poppins;
   font-size: 14px;
   font-weight: normal;
-  color: #000000;
+  color: #000;
+  z-index: 2;
 `;
 
 const ContactInformation1 = styled.div`
@@ -75,7 +86,8 @@ const ContactInformation1 = styled.div`
   height: 208px;
   border-radius: 4px;
   border: solid 1px #d9d9d9;
-  background-color: #ffffff;
+  background-color: #fff;
+  z-index: 2;
 `;
 
 const ContactInformation = styled(ContactInformation1)`
@@ -85,29 +97,32 @@ const ContactInformation = styled(ContactInformation1)`
 const Mail = styled.div`
   display: inline-block;
   margin-right: 13px;
-  width: 64px;
-  height: 24px;
+  // width: 64px;
+  // height: 24px;
   text-align: right;
-  font-family: Poppins-Regular;
+  font-family: Poppins, sans-serif;
   font-size: 16px;
   color: #229c7f;
 `;
 
-const Adresa = styled.div`
+const Adresa = styled.a`
   display: inline;
   width: 168px;
-  font-family: Poppins-Regular;
+  font-family: Poppins, sans-serif;
   font-size: 14px;
-  font-weight: normal;
-  color: #000000;
+  color: #000;
   margin-left: 13px;
 `;
 
 const Container = styled.div`
   margin-left: 208px;
-  padding-top: 32px;
-  width: 1024px;
-  height: 496px;
+  //   padding-top: 32px;
+  //   width: 1024px;
+  //   height: 496px;
+  //  ;
+  display: flex;
+  flex-direction: column;
+  align-self: center;
 `;
 
 const ContactBox = styled.div`
