31bd4774448810cea90a302f5f0acfef7fc5c5ce
antonio.hadrovic <hadrovic1@gmail.com>
2019-06-14 17:23:22 +0200

    Add manual code fixes from merge conflict

--- src/UI/citizens/components/edit-event-info/CitizensEditEventInfo.js
+++ src/UI/citizens/components/edit-event-info/CitizensEditEventInfo.js
@@ -15,47 +15,111 @@ import {
   FormInlineLabel,
   FormInlineInput
 } from './CitizensEditEventInfoStyle';
+//OLD CODE
+// const CitizensEditEventInfo = () => (
+//   <MainContainer>
+//     <CricleMark>
+//       <NumberMark>3</NumberMark>
+//     </CricleMark>
+//     <MainHeader>Podaci o događaju i Vama</MainHeader>
+//     <MainDescription>
+//       Recite nam ime svog događaja i pobliže ga objasnite. Nemojte zaboraviti
+//       ostaviti svoje kontakte podatke kako bismo Vas mogli kontaktirati u vezi
+//       dodatnih detalja i potvrđivanja rezervacije.
+//     </MainDescription>
+//     <FormContainer>
+//       <FormHeader>Naziv događaja:</FormHeader>
+//       <FormInput />
+//       <FormHeader>Opis događaja:</FormHeader>
+//       <FormInputText placeholder="OPIŠITE SVOJ DOGAĐAJ..." />
+//       <FormInputTextCharacterCounter>
+//         0 / 500 znakova
+//       </FormInputTextCharacterCounter>
+//       <FormHeader>Vaši kontakt podaci:</FormHeader>
+//       <FormContainerInfoGroup>
+//         <FormInlineInput>
+//           <FormInlineLabel>Ime i prezime</FormInlineLabel>
+//           <FormInput />
+//         </FormInlineInput>
+//         <FormInlineInput>
+//           <FormInlineLabel>Organizacija (neobavezno)</FormInlineLabel>
+//           <FormInput />
+//         </FormInlineInput>
+//         <FormInlineInput>
+//           <FormInlineLabel>E-mail adresa</FormInlineLabel>
+//           <FormInput />
+//         </FormInlineInput>
+//         <FormInlineInput>
+//           <FormInlineLabel>Broj telefona/Mobitela</FormInlineLabel>
+//           <FormInput />
+//         </FormInlineInput>
+//       </FormContainerInfoGroup>
+//     </FormContainer>
+//   </MainContainer>
+// );
+//OLD CODE END
 
-const CitizensEditEventInfo = () => (
-  <MainContainer>
-    <CricleMark>
-      <NumberMark>3</NumberMark>
-    </CricleMark>
-    <MainHeader>Podaci o događaju i Vama</MainHeader>
-    <MainDescription>
-      Recite nam ime svog događaja i pobliže ga objasnite. Nemojte zaboraviti
-      ostaviti svoje kontakte podatke kako bismo Vas mogli kontaktirati u vezi
-      dodatnih detalja i potvrđivanja rezervacije.
-    </MainDescription>
-    <FormContainer>
-      <FormHeader>Naziv događaja:</FormHeader>
-      <FormInput />
-      <FormHeader>Opis događaja:</FormHeader>
-      <FormInputText placeholder="OPIŠITE SVOJ DOGAĐAJ..." />
-      <FormInputTextCharacterCounter>
-        0 / 500 znakova
-      </FormInputTextCharacterCounter>
-      <FormHeader>Vaši kontakt podaci:</FormHeader>
-      <FormContainerInfoGroup>
-        <FormInlineInput>
-          <FormInlineLabel>Ime i prezime</FormInlineLabel>
-          <FormInput />
-        </FormInlineInput>
-        <FormInlineInput>
-          <FormInlineLabel>Organizacija (neobavezno)</FormInlineLabel>
-          <FormInput />
-        </FormInlineInput>
-        <FormInlineInput>
-          <FormInlineLabel>E-mail adresa</FormInlineLabel>
-          <FormInput />
-        </FormInlineInput>
-        <FormInlineInput>
-          <FormInlineLabel>Broj telefona/Mobitela</FormInlineLabel>
-          <FormInput />
-        </FormInlineInput>
-      </FormContainerInfoGroup>
-    </FormContainer>
-  </MainContainer>
-);
+class CitizensEditEventInfo extends React.Component {
+  state = {
+    charCounter: 0
+  };
+
+  handleCharCount = event => {
+    const charCount = event.target.value.length;
+    const counter = this.state.charCounter;
+    const charLength = +charCount;
+    this.setState({ charCounter: charLength });
+  };
+
+  render() {
+    return (
+      <MainContainer>
+        <CricleMark>
+          <NumberMark>3</NumberMark>
+        </CricleMark>
+        <MainHeader>Podaci o događaju i Vama</MainHeader>
+        <MainDescription>
+          Recite nam ime svog događaja i pobliže ga objasnite. Nemojte
+          zaboraviti ostaviti svoje kontakte podatke kako bismo Vas mogli
+          kontaktirati u vezi dodatnih detalja i potvrđivanja rezervacije.
+        </MainDescription>
+        <FormContainer>
+          <FormHeader>Naziv događaja:</FormHeader>
+          <FormInput required />
+          <FormHeader>Opis događaja:</FormHeader>
+          <FormInputText
+            placeholder="OPIŠITE SVOJ DOGAĐAJ..."
+            type="text"
+            maxLength="500"
+            onChange={this.handleCharCount}
+            required
+          />
+          <FormInputTextCharacterCounter>
+            {this.state.charCounter} / 500 znakova
+          </FormInputTextCharacterCounter>
+          <FormHeader>Vaši kontakt podaci:</FormHeader>
+          <FormContainerInfoGroup>
+            <FormInlineInput>
+              <FormInlineLabel>Ime i prezime</FormInlineLabel>
+              <FormInput required />
+            </FormInlineInput>
+            <FormInlineInput>
+              <FormInlineLabel>Organizacija (nije obavezno)</FormInlineLabel>
+              <FormInput />
+            </FormInlineInput>
+            <FormInlineInput>
+              <FormInlineLabel>E-mail adresa</FormInlineLabel>
+              <FormInput type="email" required />
+            </FormInlineInput>
+            <FormInlineInput>
+              <FormInlineLabel>Broj telefona/Mobitela</FormInlineLabel>
+              <FormInput type="text" pattern="[0-9]*" required />
+            </FormInlineInput>
+          </FormContainerInfoGroup>
+        </FormContainer>
+      </MainContainer>
+    );
+  }
+}
 
 export default CitizensEditEventInfo;
--- src/UI/citizens/components/edit-event-info/CitizensEditEventInfoStyle.js
+++ src/UI/citizens/components/edit-event-info/CitizensEditEventInfoStyle.js
@@ -1,8 +1,145 @@
-import styled from 'styled-components';
+//OLD CODE
+// import styled from 'styled-components';
 
+// const MainContainer = styled.div`
+//   margin-top: 32px;
+//   ${'' /* margin: auto; */}
+//   width: 1024px;
+//   border-radius: 4px;
+//   border: solid 1px #d9d9d9;
+//   background-color: #ffffff;
+//   padding-left: 130px;
+//   padding-right: 130px;
+//   padding-bottom: 24px;
+// `;
+
+// const MainHeader = styled.h1`
+//   margin-top: 32px;
+//   margin-bottom: 16px;
+//   font-family: Lora;
+//   font-size: 32px;
+//   color: #000000;
+// `;
+
+// const MainDescription = styled.p`
+//   font-family: Poppins, Arial, sans-serif;
+//   font-size: 14px;
+//   color: #000000;
+//   padding-bottom: 16px;
+// `;
+
+// const FormContainer = styled.form``;
+
+// const FormHeader = styled.h5`
+//   text-transform: uppercase;
+//   font-family: Poppins, Arial, sans-serif;
+//   font-size: 16px;
+//   font-weight: normal;
+//   color: #434343;
+//   padding-top: 16px;
+// `;
+
+// const FormContainerInfoGroup = styled.form`
+//   display: flex;
+//   flex-wrap: wrap;
+//   justify-content: space-between;
+// `;
+
+// const FormInput = styled.input`
+//   width: 100%;
+//   height: 32px;
+//   border-radius: 2px;
+//   border: solid 1px rgba(0, 0, 0, 0.15);
+//   background-color: #ffffff;
+//   margin-top: 8px;
+//   margin-bottom: 8px;
+//   padding-left: 8px;
+//   padding-right: 8px;
+// `;
+
+// const FormInputText = styled.textarea`
+//   width: 100%;
+//   height: 136px;
+//   font-family: Poppins, Arial, sans-serif;
+//   font-size: 14px;
+//   font-weight: 500;
+//   padding-left: 8px;
+//   padding-top: 6px;
+//   padding-right: 8px;
+//   padding-bottom: 6px;
+//   margin-top: 8px;
+//   margin-bottom: 4px;
+//   border-radius: 2px;
+//   border: solid 1px rgba(0, 0, 0, 0.15);
+//   background-color: #ffffff;
+//   resize: none;
+// `;
+
+// const FormInputTextCharacterCounter = styled.p`
+//   opacity: 0.5;
+//   font-family: Poppins, Arial, sans-serif;
+//   font-size: 12px;
+//   font-weight: 200;
+//   color: #000000;
+//   text-align: end;
+// `;
+
+// const FormInlineLabel = styled.h6`
+//   text-transform: uppercase;
+//   opacity: 0.5;
+//   font-family: Poppins, Arial, sans-serif;
+//   font-size: 12px;
+//   font-weight: 200;
+//   color: #000000;
+//   margin-top: 8px;
+// `;
+
+// const FormInlineInput = styled.span`
+//   display: inline-block;
+//   width: 374px;
+// `;
+
+// const CricleMark = styled.div`
+//   width: 152px;
+//   height: 152px;
+//   opacity: 0.4;
+//   border: solid 16px rgba(130, 212, 167, 0.4);
+//   border-radius: 50%;
+//   position: absolute;
+//   margin-top: 16px;
+//   margin-left: -114px;
+// `;
+
+// const NumberMark = styled.div`
+//   position: absolute;
+//   font-family: Poppins, Arial, sans-serif;
+//   font-size: 96px;
+//   font-weight: 800;
+//   text-align: center;
+//   margin-left: 35px;
+//   color: #82d4a7;
+// `;
+
+// export {
+//   MainContainer,
+//   MainHeader,
+//   MainDescription,
+//   FormContainer,
+//   FormHeader,
+//   FormContainerInfoGroup,
+//   FormInput,
+//   FormInputText,
+//   FormInputTextCharacterCounter,
+//   FormInlineLabel,
+//   FormInlineInput,
+//   CricleMark,
+//   NumberMark
+// };
+//OLD CODE END
+
+import styled from 'styled-components';
 const MainContainer = styled.div`
   margin-top: 32px;
-  ${'' /* margin: auto; */}
   width: 1024px;
   border-radius: 4px;
   border: solid 1px #d9d9d9;
@@ -13,6 +150,7 @@ const MainContainer = styled.div`
 `;
 
 const MainHeader = styled.h1`
+  line-height: normal;
   margin-top: 32px;
   margin-bottom: 16px;
   font-family: Lora;
@@ -21,15 +159,16 @@ const MainHeader = styled.h1`
 `;
 
 const MainDescription = styled.p`
+  line-height: normal;
   font-family: Poppins, Arial, sans-serif;
   font-size: 14px;
   color: #000000;
   padding-bottom: 16px;
 `;
-
 const FormContainer = styled.form``;
 
 const FormHeader = styled.h5`
+  line-height: normal;
   text-transform: uppercase;
   font-family: Poppins, Arial, sans-serif;
   font-size: 16px;
@@ -37,13 +176,11 @@ const FormHeader = styled.h5`
   color: #434343;
   padding-top: 16px;
 `;
-
 const FormContainerInfoGroup = styled.form`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 `;
-
 const FormInput = styled.input`
   width: 100%;
   height: 32px;
@@ -54,6 +191,9 @@ const FormInput = styled.input`
   margin-bottom: 8px;
   padding-left: 8px;
   padding-right: 8px;
+  font-family: Poppins, Arial, sans-serif;
+  font-size: 14px;
+  opacity: 0.5;
 `;
 
 const FormInputText = styled.textarea`
@@ -72,6 +212,7 @@ const FormInputText = styled.textarea`
   border: solid 1px rgba(0, 0, 0, 0.15);
   background-color: #ffffff;
   resize: none;
+  opacity: 0.5;
 `;
 
 const FormInputTextCharacterCounter = styled.p`
@@ -82,7 +223,6 @@ const FormInputTextCharacterCounter = styled.p`
   color: #000000;
   text-align: end;
 `;
-
 const FormInlineLabel = styled.h6`
   text-transform: uppercase;
   opacity: 0.5;
@@ -92,33 +232,29 @@ const FormInlineLabel = styled.h6`
   color: #000000;
   margin-top: 8px;
 `;
-
 const FormInlineInput = styled.span`
   display: inline-block;
   width: 374px;
 `;
-
 const CricleMark = styled.div`
   width: 152px;
   height: 152px;
-  opacity: 0.4;
+  opacity: 0.2;
   border: solid 16px rgba(130, 212, 167, 0.4);
   border-radius: 50%;
   position: absolute;
   margin-top: 16px;
   margin-left: -114px;
 `;
-
 const NumberMark = styled.div`
   position: absolute;
   font-family: Poppins, Arial, sans-serif;
   font-size: 96px;
   font-weight: 800;
   text-align: center;
-  margin-left: 35px;
+  margin-left: 34px;
   color: #82d4a7;
 `;
-
 export {
   MainContainer,
   MainHeader,
