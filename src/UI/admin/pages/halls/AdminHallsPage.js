c89c09a8bc65a509e4182917c9e6905ef7a8a427
Tomislav Koretic <opel.tigra16@gmail.com>
2019-06-16 15:11:43 +0200

    Add API,Fetch,DB

--- src/UI/admin/pages/halls/AdminHallsPage.js
+++ src/UI/admin/pages/halls/AdminHallsPage.js
@@ -5,15 +5,45 @@ import { Footer, Modal } from '../../../common';
 import AdminAvailableHall from '../../components/available-hall/AdminAvailableHall';
 import AdminAddHall from '../../components/add-hall/AdminAddHall';
 import { AdminHallPageContainer } from '../../components/available-hall/AdminAvailableHallStyle';
-const AdminHallsPage = () => (
-  <React.Fragment>
-    <AdminHeader />
-    <AdminHallPageContainer>
-      <AdminAvailableHall />
-      <AdminAddHall />
-    </AdminHallPageContainer>
-    <Footer />
-  </React.Fragment>
-);
+class <AdminAvailableHall extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+    this.FetchRequests = this.FetchRequests.bind(this);
+  }
+  FetchRequests() {
+    fetch(API_BASE_URL + '/api/halls')
+      .then(response => response.json())
+      .then(data => this.setState({ data }))
+      .catch(error => console.log(error));
+  }
+  componentDidMount() {
+    this.FetchRequests();
+  }
+  render() {
+    let items = this.state.data;
+    return (
+      <React.Fragment>
+        <div style={{ minHeight: 'calc(100vh - 160px)' }}>
+          <AdminHeader />
+          {items.map(item => (
+            <AdminAvailableHall
+              key={item.id}
+              titleHall={item.hall.name}
+              hallImagel={item.hall.pictureUrl}
+              locationText={item.hall.address}
+              descriptionText={item.hall.description}
+              id={item.id}
+              updatePage={this.FetchRequests}
+            />
+          ))}
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
 
 export default AdminHallsPage;
