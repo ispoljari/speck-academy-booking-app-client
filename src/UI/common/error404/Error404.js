60d1c587efc090eaf9a040313f432b76c26b4a1e
ispoljari <ivanspoljaric22@gmail.com>
2019-06-16 15:10:57 +0200

    Fix 404Page and implement routing functionality

--- src/UI/common/error404/Error404.js
+++ src/UI/common/error404/Error404.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import { Body, Container, Picture, Error, Button } from './Error404Style';
+import { Body, Container, Picture, Error, LinkContent } from './Error404Style';
 
 const Error404 = () => (
   <Body>
     <Container>
       <Picture />
       <Error>Greška 404 - stranica nije pronađena</Error>
-      <Button>POVRATAK</Button>
+      <NavLink to="/">
+        <LinkContent>Povratak</LinkContent>
+      </NavLink>
     </Container>
   </Body>
 );
